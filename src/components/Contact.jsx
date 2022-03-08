fcf8d71d401ef8d2261363300fccb13341c43063
Jacob Wachtel <jawachtel@gmail.com>
2022-03-08 12:43:15 -0500

    Contact props assignment updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 
 // TODO: Pass down props as a parameter and take the parts of object you pass down to dyanmically create a unique contact for each component
 
-function Contact() {
+function Contact({ image, name, phone, icon}) {
     return (
         <div className='contactContainer'>
+            <div className="container__icon">{icon}</div>
             <div className="imageContainer">
-                <img src="https://economictimes.indiatimes.com/thumb/msid-84588036,width-1200,height-900,resizemode-4,imgsize-109325/elon-musk.jpg?from=mdr"alt=""></img>
+                <img src={image} alt=""></img>
             </div>
-
+            
             <div className="contactInfo">
-                <h2>Elon Musk</h2>
-                <h2>1-888-518-3752.</h2>
+                <h2>{name}</h2>
+                <h2>{phone}</h2>
             </div>
         </div>
     )
--- src/components/Contactslist.jsx
+++ src/components/Contactslist.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import Contact from './Contact'
 
-function Contactslist() {
+
+
+function Contactslist({contactArray}) {
+
+    
+
+
     return (
         <div className='Contactslist'>
             <div className="title">
                 <h1>Contacts</h1>
             </div>
-            
-         <Contact/>
+            <div className="contactArrays">
+                {contactArray.map((contact, index) => (
+                <Contact
+                    key={index}
+                    image={contact.image}
+                    name={contact.name}
+                    phone={contact.phone}
+                    icon={contact.icon}
+                />
+                ))}
+         </div>
         </div>
     )
 }
